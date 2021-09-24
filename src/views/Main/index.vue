8961c1d4a5d9033ffc0be781d845e3750134489f
wypyp <109807001@qq.com>
2021-09-24 15:17:29 +0800

    test

--- src/views/Main/index.vue
+++ src/views/Main/index.vue
@@ -1,8 +1,16 @@
+<!--
+ * @Author: WangYP
+ * @Date: 2021-09-22 15:12:44
+ * @LastEditors: ZhouJG
+ * @LastEditTime: 2021-09-24 15:15:02
+ * @Description: 描述信息
+ * @FilePath: /vuecli/src/views/Main/index.vue
+-->
 <template>
   <div class="main-page-wrapper">
-    <div>你可能需要一个头</div>
+    <div>头部</div>
     <router-view />
-    <div>你可能需要一个尾</div>
+    <div>尾部</div>
   </div>
 </template>
 
